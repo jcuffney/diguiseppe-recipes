19570261d288ad7edd393e5331f7541d50c91432
Joe Cuffney <jcuffney@datto.com>
2019-11-01 19:39:00 -0400

    added in mocha

---
+++ src/test/lib.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+
+describe('lib', () => {
+  describe('readFile', () => {
+    it('throws when no file at path', () => {
+      assert.equal(true, false);
+    });
+
+    it('returns JSON when file exists', () => {
+      assert.equal(true, false);
+    });
+  });
+});
