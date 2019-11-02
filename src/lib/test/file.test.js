2e87f5dd97ac571e6d32745c7ce4c304789ebacf
Joe Cuffney <jcuffney@datto.com>
2019-11-02 07:42:08 -0400

    testing coverage w/ code climate

--- src/lib/test/file.test.js
+++ src/lib/test/file.test.js
@@ -1,8 +1,10 @@
 const assert = require('assert');
+const { readFile, writeFile } = require('../file');
 
 describe('lib', () => {
   describe('readFile', () => {
     it('returns JSON when file exists', () => {
+      readFile('./data/recipes.json');
       assert.equal(true, true);
     });
   });
