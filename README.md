dc2fd4375b6c4c5ca038fb3baf6859a247cbb7cd
Joe Cuffney <jcuffney@datto.com>
2019-11-04 10:43:41 -0500

    dockerized

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 - Apollo Server
 - React Website
+
+## Requirements
+
+- Docker
+
+## Getting Started
+
+- `docker-compose up`
+- open: http://localhost:4000 for graphql playground
+- open: http://localhost for front end.
--- services/graphql-gateway/src/index.js
+++ services/graphql-gateway/src/index.js
@@ -3,16 +3,14 @@ const { ApolloGateway } = require('@apollo/gateway');
 const express = require('express');
 
 const serviceList = [
-  { name: 'recipe', url: 'http://localhost:4001/' },
-  { name: 'user', url: 'http://localhost:4002/' },
-  { name: 'media', url: 'http://localhost:4003/' },
+  { name: 'recipe', url: 'http://recipe_graphql:4001/' },
 ];
 
 (async () => {
   const app = express();
   const gateway = new ApolloGateway({ serviceList });
   const { schema, executor } = await gateway.load();
-  const server = new ApolloServer({ schema, executor });
+  const server = new ApolloServer({ schema, executor, introspection: true, playground: true });
   server.applyMiddleware({ app, path: '/' });
   app.listen({ port: 4000 });
 })();
