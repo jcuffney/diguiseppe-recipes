9a09fb47866899e445bf16716609756663964479
Joe Cuffney <jcuffney@datto.com>
2019-11-06 19:59:28 -0500

    FE can now query backend

--- views/diguisepperecipes.com/src/apolloClient.js
+++ views/diguisepperecipes.com/src/apolloClient.js
@@ -1,5 +1,15 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient, { InMemoryCache }  from 'apollo-boost';
 
 export const client = new ApolloClient({
-  uri: 'http://graphql:4000',
+  uri: 'http://localhost:4000/graphql',
+  request: (operation) => {
+    const token = 'test';
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
 });
